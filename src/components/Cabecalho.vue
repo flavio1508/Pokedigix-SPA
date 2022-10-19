3a0fd30751b66880618d4ae3697875d53c1b8ed5
Flávio Augusto <flavioaugusto1508@gmail.com>
2022-10-19 17:43:45 -0400

    Adicionando cokies

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -21,7 +21,7 @@
     <nav class="sticky-top navbar navbar-dark navbar-expand-lg bg-dark">
       <div class="container-md">
         <a class="navbar-brand" href="#">Pokedigix</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button  class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
